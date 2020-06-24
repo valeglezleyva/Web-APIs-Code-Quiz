d05130adef7263dfc476e0df85dee37a3e9123ca
Valeria Gonzalez Leyva <valeglez.leyva@gmail.com>
2020-06-23 22:36:35 -0500

    Added  initial questions.js file

---
+++ questions.js
@@ -0,0 +1,29 @@
+var questions = [
+	{
+		title: "Question 1",
+		choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+		answer: "alerts"
+	},
+	{
+		title: "Question 2",
+		choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+		answer: "Answer 1"
+    },
+    {
+		title: "Question 3",
+		choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+		answer: "Answer 2"
+    },
+    {
+		title: "Question 4",
+		choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+		answer: "Answer 3"
+    },
+    {
+		title: "Question 5",
+		choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+		answer: "Answer 4"
+    }
+]
+
+// h2 attr style = "bold"
